c3bcdecf4f6b625a8ddfe75c50c2955f4bb80d76
Ash Savigne <ashleysavigne@gmail.com>
2023-10-03 10:08:20 -0400

    Update AdjacencyList.h

--- AdjacencyList.h
+++ AdjacencyList.h
@@ -1,1 +1,29 @@
+class Graph {
+public:
+
+    struct Node{
+        // Node class variables
+        string name;
+        forward_list<Node*> inNodes;
+        int out;
+        double r;
+        double pr;
+
+        explicit Node(string name) : name(std::move(name)), r(0), inNodes(), pr(0), out(0) {}
+
+    };
+private:
+    // Graph class variables
+    map<string, Node*> vertices;
+public:
+    // Constructor
+    Graph() : vertices() {}
+
+    void insertEdge(const string& to, const string& from);
+    void PageRank(int n);
+    void printGraph();
+
+    // Destructor
+    ~Graph();
+};
 
